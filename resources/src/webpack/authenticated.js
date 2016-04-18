229dcf352e805d4ec76fc1b971a823e94d3c4b28
Asko Soukka <asko.soukka@iki.fi>
2016-04-18 15:22:15 +0300

    Cleanup

--- resources/src/webpack/authenticated.js
+++ resources/src/webpack/authenticated.js
@@ -3,7 +3,6 @@ import 'plonetheme.webpack/plone-logged-in';
 import 'plonetheme.webpack/plone-mosaic';
 import 'plonetheme.webpack/plone-mosaic-layouts-editor';
 import 'plonetheme.webpack/products-ploneformgen';
-// import './license-selector';
 import './styles.less';
 import './styles.scss';
 
