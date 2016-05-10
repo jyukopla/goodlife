20128be89b4ca10376514a42eb23aaa29b0a5a33
Erno Tuovinen <erno.tuovinen@gmail.com>
2016-05-10 13:39:24 +0300

    Fixed Slick SCSS and JS imports to import files from node_modules

--- resources/src/webpack/anonymous.js
+++ resources/src/webpack/anonymous.js
@@ -2,5 +2,5 @@ import 'plonetheme.webpack/plone';
 import './styles.less';
 import './styles.scss';
 
-import './js/slick.min.js';
+
 import './js/main.js';
--- resources/src/webpack/authenticated.js
+++ resources/src/webpack/authenticated.js
@@ -5,7 +5,7 @@ import 'plonetheme.webpack/plone-mosaic-layouts-editor';
 import './styles.less';
 import './styles.scss';
 
-import './js/slick.min.js';
+
 
 import './js/main.js';
 
