b8c8e5a933508142310ffb7b5cfcbbe061b98209
Erno Tuovinen <erno.tuovinen@gmail.com>
2016-05-11 12:27:40 +0300

    Fix: Fixed slick carousel import call, changed location to main.js (jquery dependency matters), removed next and preview buttons, setted null to slick button settings

--- resources/src/webpack/anonymous.js
+++ resources/src/webpack/anonymous.js
@@ -2,6 +2,6 @@ import 'plonetheme.webpack/plone';
 import './styles.less';
 import './styles.scss';
 
-import '../../node_modules/slick-carousel/slick/slick.min.js';
+
 
 import './js/main.js';
--- resources/src/webpack/js/main.js
+++ resources/src/webpack/js/main.js
@@ -1,5 +1,12 @@
 import jQuery from 'jquery';
 
+
+// This imports Slick Carousel JS from NPM modules
+// Slick needs jQuery as an dependency, that is the reason
+// why this is imported here, not in authenticate.js file
+
+import 'slick-carousel';
+
 jQuery(($) => {
 
   $(document).ready(function()
@@ -14,8 +21,8 @@ jQuery(($) => {
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        nextArrow: '<button type="button" class="slick-next"></button>',
-        prevArrow: '<button type="button" class="slick-prev"></button>'
+        nextArrow: null, //'<button type="button" class="slick-next"></button>', // null to remove
+        prevArrow: null, //'<button type="button" class="slick-prev"></button>', // null to remove
       });
   });
 
