e09ed4dd22b941b16f7f713235bb006bffdc78ba
Erno Tuovinen <erno.tuovinen@gmail.com>
2016-05-11 10:04:01 +0300

    Edit: Added some jQuery to format carousel next and prev buttons, added support scss styles for buttons

--- resources/src/webpack/js/main.js
+++ resources/src/webpack/js/main.js
@@ -4,13 +4,18 @@ jQuery(($) => {
 
   $(document).ready(function()
   {
+
+    // This controls the slick carousel, add additional settings here
+    // https://github.com/kenwheeler/slick/
     $('.carousel-test').slick({
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 4000,
         infinite: true,
         speed: 500,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        nextArrow: '<button type="button" class="slick-next"></button>',
+        prevArrow: '<button type="button" class="slick-prev"></button>'
       });
   });
 
