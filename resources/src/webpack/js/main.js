1a413e9b8280f0310f56f1811e6ca468ad893387
Erno Tuovinen <erno.tuovinen@gmail.com>
2016-05-11 09:12:02 +0300

    Added: OK logos, Edit: Edited carousel funcitonality

--- resources/src/webpack/js/main.js
+++ resources/src/webpack/js/main.js
@@ -5,7 +5,12 @@ jQuery(($) => {
   $(document).ready(function()
   {
     $('.carousel-test').slick({
-
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
       });
   });
 
