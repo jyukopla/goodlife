c82c6a8b0b69252e0f0542541fb628d6dcd415b4
Asko Soukka <asko.soukka@iki.fi>
2016-05-26 10:45:22 +0300

    Cleanup

--- resources/src/webpack/anonymous.js
+++ resources/src/webpack/anonymous.js
@@ -1,5 +1,4 @@
 import 'plonetheme.webpack/plone';
-// import 'collective.dynapage';
 import './styles.less';
 import './styles.scss';
 
--- resources/src/webpack/authenticated.js
+++ resources/src/webpack/authenticated.js
@@ -2,7 +2,6 @@ import 'plonetheme.webpack/plone';
 import 'plonetheme.webpack/plone-logged-in';
 import 'plonetheme.webpack/plone-mosaic';
 import 'plonetheme.webpack/plone-mosaic-layouts-editor';
-// import 'collective.dynapage';
 import './styles.less';
 import './styles.scss';
 
@@ -11,4 +10,3 @@ import './js/main.js';
 
 import jQuery from 'jquery';
 window.jQuery = jQuery;
-0
